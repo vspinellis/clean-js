095ffe4a6cfd3eb75a766f21d071a17b1ac8fcf0
Vinicius Spinellis <vspinellis@hotmail.com>
2024-03-07 16:39:25 -0300

    cria o teste de buscarPorCPF no usuariosRepository

--- reuniao.md
+++ reuniao.md
@@ -36,6 +36,7 @@
 
 ## UsuariosRepository
 [X] cadastrar: ({nome_completo, CPF, telefone, endereco, email}) => Promise<void>
+[] buscarPorCPF (CPF) => Promise<Usuario | null>
 [] existePorCPF(CPF) => Promise<boolean>
 [] existePorEmail(email) => Promise<boolean>
 
--- src/infra/db/typeorm/repositories/usuarios.repository.spec.js
+++ src/infra/db/typeorm/repositories/usuarios.repository.spec.js
@@ -1,6 +1,10 @@
-const { usuariosRepository } = require('./usuarios.repository');
+const { usuariosRepository, typeormUsuariosRepository } = require('./usuarios.repository');
 
 describe('Usuarios Repository', function () {
+  beforeEach(async function () {
+    await typeormUsuariosRepository.delete({});
+  });
+
   test('Deve retornar void ao criar um usuario', async function () {
     const sut = usuariosRepository();
     const usuarioCriado = await sut.cadastrar({
@@ -12,5 +16,20 @@ describe('Usuarios Repository', function () {
     });
 
     expect(usuarioCriado).toBeUndefined();
+  });
+
+  test('Deve retornar um usuário se o mesmo existir buscando por CPF', async function () {
+    await typeormUsuariosRepository.save({
+      nome_completo: 'nome_valido',
+      CPF: 'CPF_valido',
+      telefone: 'telefone_valido',
+      email: 'email_valido',
+      endereco: 'envereco_valido'
+    });
+    const sut = usuariosRepository();
+    const buscarPorCPFCadastrado = await sut.buscarPorCPF('CPF_valido');
+
+    expect(buscarPorCPFCadastrado.id).toBeDefined();
+    expect(buscarPorCPFCadastrado.nome_completo).toBe('nome_valido');
   });
 });
