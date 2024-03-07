3983a13aa925f1178160d51aece0361662a33a3d
Vinicius Spinellis <vspinellis@hotmail.com>
2024-03-07 16:16:41 -0300

    cria o teste de cadastrar o usuario no repositorio

---
+++ src/infra/db/typeorm/repositories/usuarios.repository.spec.js
@@ -0,0 +1,14 @@
+describe('Usuarios Repository', function () {
+  test('Deve retornar void ao criar um usuario', async function () {
+    const sut = usuariosRepository();
+    const usuarioCriado = await sut.cadastrar({
+      nome_completo: 'nome_valido',
+      CPF: 'CPF_valido',
+      telefone: 'telefone_valido',
+      email: 'email_valido',
+      endereco: 'envereco_valido'
+    });
+
+    expect(usuarioCriado).toBeUndefined();
+  });
+});
