b56e567c0d5c8ccadc55609a1dac8b0e76d944be
Vinicius Spinellis <vspinellis@hotmail.com>
2024-03-07 13:04:11 -0300

    cria o teste de retorno com multa na entity emprestimos

--- src/enterprise/entities/emprestimo.entity.spec.js
+++ src/enterprise/entities/emprestimo.entity.spec.js
@@ -9,4 +9,13 @@ describe('Emprestimos Entity', function () {
 
     expect(resultado).toBe('Multa por atraso: R$ 0');
   });
+
+  test('calcularMulta - com atraso', function () {
+    const resultado = emprestimoEntity.calcularMulta({
+      data_devolucao: '2024-02-17',
+      data_retorno: '2024-02-16'
+    });
+
+    expect(resultado).toBe('Multa por atraso: R$ 10,00');
+  });
 });
