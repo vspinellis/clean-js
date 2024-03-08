918b1e3914ee1df4ebdce651dd1a1d71cd58885c
Vinicius Spinellis <vspinellis@hotmail.com>
2024-03-08 18:45:46 -0300

    controller cadastrar livro

---
+++ src/interface-adapters/controllers/cadastrar-livro.controller.js
@@ -0,0 +1,37 @@
+const { z } = require('zod');
+const httpResponse = require('../../shared/helpers/http.response');
+
+const zodValidator = z.object({
+  nome: z.string({
+    required_error: 'Nome é obrigatório'
+  }),
+  quantidade: z.number({
+    required_error: 'Quantidade é obrigatória'
+  }),
+  autor: z.string({
+    required_error: 'Autor é obrigatório'
+  }),
+  genero: z.string({
+    required_error: 'Gênero é obrigatório'
+  }),
+  ISBN: z.string({
+    required_error: 'ISBN é obrigatório'
+  })
+});
+
+module.exports = async function cadastrarLivroController({ cadastrarLivroUseCase, httpRequest }) {
+  const { nome, quantidade, autor, genero, ISBN } = zodValidator.parse(httpRequest.body);
+
+  const output = await cadastrarLivroUseCase({
+    nome,
+    quantidade,
+    autor,
+    genero,
+    ISBN
+  });
+
+  return output.fold(
+    (error) => httpResponse(400, error.message),
+    () => httpResponse(201, null)
+  );
+};
