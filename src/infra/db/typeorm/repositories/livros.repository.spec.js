2f9da2965e7e51710c9470e782d5ae50a7d258dc
Vinicius Spinellis <vspinellis@hotmail.com>
2024-03-07 22:31:18 -0300

    cria o livrosRepository.spec.js e cria o primeiro teste de void ao cadastrar

---
+++ src/infra/db/typeorm/repositories/livros.repository.spec.js
@@ -0,0 +1,21 @@
+describe('Livros Repository Typeorm', function () {
+  let sut;
+
+  beforeAll(function () {
+    sut = livrosRepository();
+  });
+
+  const livroDTO = {
+    nome: 'nome_valido',
+    quantidade: 3,
+    autor: 'autor_valido',
+    genero: 'genero_valido',
+    ISBN: 'ISBN_valido'
+  };
+
+  test('Deve retornar void ao criar um livro', async function () {
+    const livroCriado = await sut.cadastrar(livroDTO);
+
+    expect(livroCriado).toBeUndefined();
+  });
+});
