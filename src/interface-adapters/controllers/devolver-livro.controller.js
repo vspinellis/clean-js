865b74dfc9367f15de5e80b8883cc3843f8f95e0
Vinicius Spinellis <vspinellis@hotmail.com>
2024-03-09 12:12:37 -0300

    cria o controller devolerLivroController

---
+++ src/interface-adapters/controllers/devolver-livro.controller.js
@@ -0,0 +1,23 @@
+const { z } = require('zod');
+const httpResponse = require('../../shared/helpers/http.response');
+
+const zodValidatorBody = z.object({
+  data_devolucao: z.string({
+    required_error: 'Date devolução é obrigatória'
+  })
+});
+
+module.exports = async function devolverLivroController({ devolverLivroUseCase, httpRequest }) {
+  const { data_devolucao } = zodValidatorBody.parse(httpRequest.body);
+  const { emprestimo_id } = httpRequest.params;
+
+  const output = await devolverLivroUseCase({
+    emprestimo_id,
+    data_devolucao: new Date(data_devolucao)
+  });
+
+  return output.fold(
+    (error) => httpResponse(400, error.message),
+    (resultado) => httpResponse(200, resultado)
+  );
+};
