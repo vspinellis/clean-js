0c0bc14b5dda18d93dd933be17c0b63bc1bfce5f
Vinicius Spinellis <vspinellis@hotmail.com>
2024-03-09 11:48:51 -0300

    cria o teste que verifica se o controller retorna o Either.left corretamente

--- src/interface-adapters/controllers/emprestar-livro.controller.spec.js
+++ src/interface-adapters/controllers/emprestar-livro.controller.spec.js
@@ -29,4 +29,30 @@ describe('Emprestar livro Controller', function () {
     });
     expect(emprestarLivroUseCase).toHaveBeenCalledTimes(1);
   });
+
+  test('deve retornar um httpResponse 400 e error.message se o emprestimo não for realizado com sucesso por lógica do useCase', async function () {
+    emprestarLivroUseCase.mockResolvedValue(Either.Left({ message: 'validacao_invalida' }));
+    const httpRequest = {
+      body: {
+        livro_id: 1,
+        usuario_id: 1,
+        data_saida: '2024-02-03',
+        data_retorno: '2024-02-15'
+      }
+    };
+
+    const response = await emprestarLivroController({
+      emprestarLivroUseCase,
+      httpRequest
+    });
+
+    expect(response).toEqual(httpResponse(400, 'validacao_invalida'));
+    expect(emprestarLivroUseCase).toHaveBeenCalledWith({
+      livro_id: 1,
+      usuario_id: 1,
+      data_saida: expect.any(Date),
+      data_retorno: expect.any(Date)
+    });
+    expect(emprestarLivroUseCase).toHaveBeenCalledTimes(1);
+  });
 });
