0984a276ea67a20953ea05a62567ec0d002a12be
Vinicius Spinellis <vspinellis@hotmail.com>
2024-03-10 11:53:08 -0300

    cria o teste que verifica se a multa irá vir zerada

--- src/main/api/routes/emprestimos.routes.test.js
+++ src/main/api/routes/emprestimos.routes.test.js
@@ -18,23 +18,25 @@ describe('Emprestimos Routes', function () {
     await typeormUsuariosRepository.query('DELETE FROM usuarios');
   });
 
+  const livroDTO = {
+    nome: 'qualquer_nome',
+    quantidade: 3,
+    autor: 'qualquer_autor',
+    genero: 'qualquer_genero',
+    ISBN: 'qualquer_ISBN'
+  };
+
+  const usuarioDTO = {
+    nome_completo: 'qualquer_nome',
+    CPF: '123.123.123-12',
+    endereco: 'qualquer_endereco',
+    telefone: 'qualquer_telefone',
+    email: 'qualquer_email@mail.com'
+  };
+
   test('Deve ser possivel emprestar um livro', async function () {
-    const livro = await typeormLivrosRepository.save({
-      nome: 'qualquer_nome',
-      quantidade: 3,
-      autor: 'qualquer_autor',
-      genero: 'qualquer_genero',
-      ISBN: 'qualquer_ISBN'
-    });
-
-    const usuario = await typeormUsuariosRepository.save({
-      nome_completo: 'qualquer_nome',
-      CPF: '123.123.123-12',
-      endereco: 'qualquer_endereco',
-      telefone: 'qualquer_telefone',
-      email: 'qualquer_email@mail.com'
-    });
-
+    const livro = await typeormLivrosRepository.save(livroDTO);
+    const usuario = await typeormUsuariosRepository.save(usuarioDTO);
     const { statusCode, body } = await request(app).post('/emprestimos').send({
       livro_id: livro.id,
       usuario_id: usuario.id,
@@ -44,5 +46,25 @@ describe('Emprestimos Routes', function () {
 
     expect(statusCode).toBe(201);
     expect(body).toBeNull();
+  });
+
+  test('Deve retornar 200 e uma mensagem de multa não aplicada', async function () {
+    const livro = await typeormLivrosRepository.save(livroDTO);
+    const usuario = await typeormUsuariosRepository.save(usuarioDTO);
+    const emprestimo = await typeormEmprestimosRepository.save({
+      livro_id: livro.id,
+      usuario_id: usuario.id,
+      data_saida: '2024-02-15',
+      data_retorno: '2024-02-16'
+    });
+
+    const { statusCode, body } = await request(app)
+      .put(`/emprestimos/devolver/${emprestimo.id}`)
+      .send({
+        data_devolucao: '2024-02-16'
+      });
+
+    expect(statusCode).toBe(200);
+    expect(body).toBe('Multa por atraso: R$ 0');
   });
 });
