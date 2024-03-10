8e81110ff95b90a4208492ebd4373aedd50842da
Vinicius Spinellis <vspinellis@hotmail.com>
2024-03-10 00:29:06 -0300

    cria o teste que deve ser possivel buscar um livro com ISBN

--- src/main/api/routes/livros.routes.test.js
+++ src/main/api/routes/livros.routes.test.js
@@ -38,4 +38,22 @@ describe('Livros Routes', function () {
     expect(body).toHaveLength(1);
     expect(body[0]).toEqual(expect.objectContaining(livroDTO));
   });
+
+  test('Deve ser possível buscar um livro por ISBN', async function () {
+    const livroDTO = {
+      nome: 'qualquer_nome',
+      quantidade: 3,
+      autor: 'qualquer_autor',
+      genero: 'qualquer_genero',
+      ISBN: 'qualquer_ISBN'
+    };
+    await typeormLivrosRepository.save(livroDTO);
+    const { statusCode, body } = await request(app)
+      .get('/livros')
+      .query({ valor: 'qualquer_ISBN' });
+
+    expect(statusCode).toBe(200);
+    expect(body).toHaveLength(1);
+    expect(body[0]).toEqual(expect.objectContaining(livroDTO));
+  });
 });
