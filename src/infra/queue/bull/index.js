a8d9629bc26e1e9079e1723590933d0dcd5cbc72
Vinicius Spinellis <vspinellis@hotmail.com>
2024-03-10 17:27:11 -0300

    instala um painal para monitor as filas e conecta no servidor

--- src/infra/queue/bull/index.js
+++ src/infra/queue/bull/index.js
@@ -5,4 +5,8 @@ const sendMailQueue = new Queue(sendMail.key, {
   redis: { host: '127.0.0.1', port: 6379 }
 });
 
+sendMailQueue.on('failed', (job) => {
+  console.log(job);
+});
+
 module.exports = { sendMailQueue };
--- src/main/api/app.js
+++ src/main/api/app.js
@@ -3,7 +3,21 @@ const express = require('express');
 const { routes } = require('./routes');
 const { ZodError } = require('zod');
 const { typeormServer } = require('../../infra/db/typeorm/setup');
+const { createBullBoard } = require('@bull-board/api');
+const { BullAdapter } = require('@bull-board/api/bullAdapter');
+const { ExpressAdapter } = require('@bull-board/express');
+const { sendMailQueue } = require('../../infra/queue/bull');
+
 const app = express();
+
+//QUEUE MONITOR
+const serverAdapter = new ExpressAdapter();
+serverAdapter.setBasePath('/queues');
+
+createBullBoard({
+  queues: [new BullAdapter(sendMailQueue)],
+  serverAdapter
+});
 
 //CONFIGS
 app.use(express.json());
@@ -13,6 +27,8 @@ typeormServer
   .then(() => {
     //ROUTES
     app.use(routes);
+
+    app.use('/queues', serverAdapter.getRouter());
 
     app.use(function (error, request, response, next) {
       if (error instanceof ZodError) {
